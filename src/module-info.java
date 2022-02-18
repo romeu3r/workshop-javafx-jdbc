cf857e425f55e27ad2b6ff9584cb5d5a5569dd64
romeu3r <romeu3r@gmail.com>
2022-02-18 10:11:34 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,17 +1,10 @@
 module workshopjavafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
     requires javafx.graphics;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
     opens application to javafx.fxml;
+    opens gui;
     exports application;
     exports gui;
 }
